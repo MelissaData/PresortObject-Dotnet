274229aaf0fc2dbbba990919382c2c2784fe38ab
Melissa <ha@melissadata.com>
2023-04-11 14:20:46 -0700

    Commit .NET code for Presort Object

---
+++ MelissaPresortObjectWindowsDotnet/Program.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using System.ComponentModel.Design;
+using MelissaData;
+using System.Threading;
+using System.Linq;
+
+namespace MelissaPresortObjectWindowsDotnet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Variables
+      string license = "";
+      string testPresortFile = "";
+      string dataPath = "";
+
+      ParseArguments(ref license, ref testPresortFile, ref dataPath, args);
+      RunAsConsole(license, testPresortFile, dataPath);
+    }
+
+    static void ParseArguments(ref string license, ref string testPresortFile, ref string dataPath, string[] args)
+    {
+      for (int i = 0; i < args.Length; i++)
+      {
+        if (args[i].Equals("--license") || args[i].Equals("-l"))
+        {
+          if (args[i + 1] != null)
+          {
+            license = args[i + 1];
+          }
+        }
+        if (args[i].Equals("--dataPath") || args[i].Equals("-d"))
+        {
+          if (args[i + 1] != null)
+          {
+            dataPath = args[i + 1];
+          }
+        }
+        if (args[i].Equals("--file") || args[i].Equals("-f"))
+
+        {
+          if (args[i + 1] != null)
+          {
+            testPresortFile = args[i + 1];
+          }
+        }
+      }
+    }
+
+    static void RunAsConsole(string license, string testPresortFile, string dataPath)
+    {
+      Console.WriteLine("\n\n========= WELCOME TO MELISSA PRESORT OBJECT WINDOWS DOTNET =========\n");
+
+      PresortObject presortObject = new PresortObject(license, dataPath);
+
+      bool shouldContinueRunning = true;
+
+      if (presortObject.mdPresortObj.GetInitializeErrorString() != "No Errors")
+      {
+        shouldContinueRunning = false;
+      }
+
+      while (shouldContinueRunning)
+      {
+        DataContainer dataContainer = new DataContainer();
+
+        if (string.IsNullOrEmpty(testPresortFile))
+        {
+          Console.WriteLine("\nFill in each value to see the Presort Object results");
+          Console.Write("Presort file path:");
+
+          dataContainer.PresortFile = Console.ReadLine();
+          dataContainer.FormatPresortOutputFile();
+        }
+        else
+        {
+          dataContainer.PresortFile = testPresortFile;
+          dataContainer.FormatPresortOutputFile();
+        }
+
+        // Print user input
+        Console.WriteLine("\n============================== INPUTS ==============================\n");
+        Console.WriteLine($"\t         Presort File: {dataContainer.PresortFile}");
+       
+        // Execute Presort Object
+        presortObject.ExecuteObjectAndResultCodes(ref dataContainer);
+
+        // Print output
+        Console.WriteLine("\n============================== OUTPUT ==============================\n");
+        Console.WriteLine("\n\tPresort Object Information:");
+
+        List<string> sections = dataContainer.GetWrapped();
+
+        Console.WriteLine($"\t          Output File: {sections[0]}");
+
+        for (int i = 1; i < sections.Count; i++) 
+        {
+          if((i == sections.Count - 1) && sections[i].EndsWith("\\"))
+          {
+            sections[i] = sections[i].Substring(0, sections[i].Length - 1);
+          }
+          Console.WriteLine($"\t                       {sections[i]}");
+        }
+
+        bool isValid = false;
+
+        if (!string.IsNullOrEmpty(testPresortFile))
+        {
+          isValid = true;
+          shouldContinueRunning = false;
+        }
+
+        while (!isValid)
+        {
+          Console.WriteLine("\nTest another file? (Y/N)");
+          string testAnotherResponse = Console.ReadLine();
+
+          if (!string.IsNullOrEmpty(testAnotherResponse))
+          {
+            testAnotherResponse = testAnotherResponse.ToLower();
+            if (testAnotherResponse == "y")
+            {
+              isValid = true;
+            }
+            else if (testAnotherResponse == "n")
+            {
+              isValid = true;
+              shouldContinueRunning = false;
+            }
+            else
+            {
+              Console.Write("Invalid Response, please respond 'Y' or 'N'");
+            }
+          }
+        }
+      }
+      Console.WriteLine("\n============= THANK YOU FOR USING MELISSA DOTNET OBJECT ============\n");
+    }
+  }
+
+  class PresortObject
+  {
+    // Path to Presort Object data files (.dat, etc)
+    string dataFilePath;
+
+    // Create instance of Melissa Presort Object
+    //public PresortObjLib.PresortCheckClass presortObj = new PresortObjLib.PresortCheckClass();
+    public mdPresort mdPresortObj = new mdPresort();
+
+    public PresortObject(string license, string dataPath)
+    {
+      // Set license string and set path to data files  (.dat, etc)
+      mdPresortObj.SetLicenseString(license);
+      dataFilePath = dataPath;
+      mdPresortObj.SetPathToPresortDataFiles(dataFilePath);
+
+      // If you see a different date than expected, check your license string and either download the new data files or use the Melissa Updater program to update your data files.  
+      //PresortObjLib.ProgramStatus pStatus = presortObj.InitializeDataFiles();
+      mdPresort.ProgramStatus pStatus = mdPresortObj.InitializeDataFiles();
+
+      // If an issue occurred while initializing the data files, this will throw
+      if (pStatus != mdPresort.ProgramStatus.ErrorNone)
+      {
+        Console.WriteLine("Failed to Initialize Object.");
+        Console.WriteLine(pStatus);
+        return;
+      }
+
+      Console.WriteLine($"                DataBase Date: {mdPresortObj.GetDatabaseDate()}");
+      Console.WriteLine($"              Expiration Date: {mdPresortObj.GetLicenseStringExpirationDate()}");
+
+      /**
+       * This number should match with the file properties of the Melissa Object binary file.
+       * If TEST appears with the build number, there may be a license key issue.
+       */
+      Console.WriteLine($"               Object Version: {mdPresortObj.GetBuildNumber()}\n");
+    }
+
+    // This will call the functions to process the input name as well as generate the result codes
+    public void ExecuteObjectAndResultCodes(ref DataContainer data)
+    {
+      /* PreSortSettings to the desired type of Presort 
+       * The sample defaults to First Class Mailing, if you'd like to use Standard, comment out the current line and uncomment the one containing the enumeration: STD_LTR_AUTO
+       * STD_LTR_AUTO is for Standard Mail, Letter, Automation pieces
+       * FCM_LTR_AUTO = First Class Mail, Letter, Automation pieces
+       */
+      mdPresortObj.SetPreSortSettings((int)mdPresort.SortationCode.FCM_LTR_AUTO);
+      //presortObj.PreSortSettings(PresortObjLib.SortationCode.STD_LTR_AUTO);
+
+      /* Sack and Parcel Dimensions */
+      mdPresortObj.SetSackWeight("30");
+      mdPresortObj.SetPieceLength("9");
+      mdPresortObj.SetPieceHeight("4.5");
+      mdPresortObj.SetPieceThickness("0.042");
+      mdPresortObj.SetPieceWeight("1.5");
+
+      /* Mailers ID 
+       * Insert a valid 6-9 digit MailersID number
+       * If you do not have a valid Mailers ID, you can visit the USPS to apply for one. Go to usps.com and click on the 'Business Customer Gateway' link at the bottom of the page.
+       */
+      mdPresortObj.SetMailersID("123456");
+
+      /* Postage Payment Methods 
+       * If both of these functions are set to false (Default setting), then Metered Mail will be used.
+       * Permit Imprint - Mailing without affixing postage. When set to true, current mailing will use permit imprint as method of postage payment
+       */
+      // presortObj.PSPermitImprint = false; // way the mailer pays for his mailing
+
+      /*	Precanceled Stamp - Cancellation of adhesive postage, stamped envelopes, or stamped cards before mailing.
+       *	When set to true, PSPrecanceledStampValue must be set to the postage value in cents
+       */
+      //presortObj.PSPrecanceledStamp = true;
+      //presortObj.PSPrecanceledStampValue(0.10);
+
+      /* Sorting Options 
+       * PSNCOA - Uses NCOALink for move updates
+       */
+      mdPresortObj.SetPSNCOA(true);
+
+      /* Post Office of Mailing Information - This is the Post office from where it will be mailed from */
+      mdPresortObj.SetPSPostOfficeOfMailingCity("RSM");
+      mdPresortObj.SetPSPostOfficeOfMailingState("CA");
+      mdPresortObj.SetPSPostOfficeOfMailingZIP("92688");
+
+      /* Update the Parameters 
+       * Verify and validate that the following properties were set to correct ranges: SetPieceHeight, SetPieceLength, SetPieceThickness,and SetPieceWeight
+       */
+      if (mdPresortObj.UpdateParameters() == false)
+      {
+        Console.WriteLine("Parameter Error: " + mdPresortObj.GetParametersErrorString());
+        Console.WriteLine("\nAborting the program...\n");
+
+        Environment.Exit(1);
+      }
+
+      /* Add Input Records to Presort Object
+       * Parse through the input file and add each record to the Presort Object
+       */
+      StreamReader reader = null;
+      Dictionary<string, string> addressDict = new Dictionary<string, string>();
+ 
+      try
+      {
+        reader = new StreamReader(data.PresortFile);
+        string line = reader.ReadLine(); //Header row
+        line = reader.ReadLine();
+        
+        while (line != null)
+        {
+          // Pre-determined format for test file
+          string[] split = line.Split(new char[] { ',' });
+          mdPresortObj.SetRecordID(split[0]);
+          string addressInfo = split[1] + "," + split[2] + "," + split[3] + "," + split[4] + "," + split[5] + "," + split[6] + "," + split[7] + "," + split[8] + "," + split[9];
+          addressDict.Add(split[0], addressInfo);
+          mdPresortObj.SetZip(split[6]);
+          mdPresortObj.SetPlus4(split[7]);
+          mdPresortObj.SetDeliveryPointCode(split[8]);
+          mdPresortObj.SetCarrierRoute(split[9]);
+       
+          //Add record
+          if (mdPresortObj.AddRecord() == false)
+          {
+            Console.WriteLine("Error Adding Record " + split[0] + ": " + mdPresortObj.GetParametersErrorString());
+          }
+
+          line = reader.ReadLine();
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("\nError Reading File: " + ex.Message);
+        Console.WriteLine("\nAborting the program...\n");
+
+        Environment.Exit(1);
+      }
+      finally
+      {
+        if (reader != null)
+        {
+          reader.Close();
+        }
+      }
+
+      /* Do Presort - Do and check if Presort was successful, otherwise return the error.*/
+      if (mdPresortObj.DoPresort() == false)
+      {
+        Console.WriteLine("Error During Presort: " + mdPresortObj.GetParametersErrorString());
+        Console.WriteLine("\nAborting the program...\n");
+
+        Environment.Exit(1);
+      }
+
+      /* Write Results */
+      try
+      {
+        StreamWriter sw = new StreamWriter(data.OutputFile);
+
+        // Write the headers on the first line
+        sw.WriteLine("RecID,MAK,Address,Suite,City,State,Zip,Plus4,DeliveryPointCode,CarrierRoute,TrayNumber,SequenceNumber,Endorsement,BundleNumber");
+
+        // Grab the first record and continue through the file
+        bool writeback = mdPresortObj.GetFirstRecord();
+        while (writeback)
+        {
+          sw.WriteLine(mdPresortObj.GetRecordID() + "," + addressDict[mdPresortObj.GetRecordID()] + ","
+              + mdPresortObj.GetTrayNumber() + "," + mdPresortObj.GetSequenceNumber() + "," + mdPresortObj.GetEndorsementLine() + "," + mdPresortObj.GetBundleNumber());
+          writeback = mdPresortObj.GetNextRecord();
+        }
+
+        sw.Flush();
+        sw.Close();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("\nError Writing File: " + ex.Message);
+      }
+    }
+  }
+  public class DataContainer
+  {
+    public string PresortFile { get; set; } = "";
+    public string OutputFile { get; set; } = "";
+
+    public void FormatPresortOutputFile()
+    { 
+      // change the name of the output file
+      int location = PresortFile.IndexOf(".csv");
+      OutputFile = PresortFile.Substring(0, location) + "_output.csv";
+    }
+
+    public List<string> GetWrapped()
+    {
+      FileInfo file = new FileInfo(OutputFile);
+      string filePath = file.FullName;
+
+      int maxLineLength = 50;
+      string[] lines = filePath.Split(new string[] { "\\" }, StringSplitOptions.None);
+      string currentLine = "";
+      List<string> wrappedString = new List<string>();
+
+      foreach (string section in lines)
+      {
+        if ((currentLine + section).Length > maxLineLength)
+        {
+          wrappedString.Add(currentLine.Trim());
+          currentLine = "";
+        }
+
+        if(section.Contains(OutputFile))
+        {
+          currentLine += section;
+        }
+        else
+        {
+          currentLine += section + "\\";
+        }    
+      }
+
+      if (currentLine.Length > 0)
+      {
+        wrappedString.Add(currentLine.Trim());
+      }
+
+      return wrappedString;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,195 @@
+# Melissa - Presort Object Windows Dotnet
+
+## Purpose
+This code showcases the Melissa Presort Object using C#.
+
+Please feel free to copy or embed this code to your own project. Happy coding!
+
+For the latest Melissa Presort Object release notes, please visit https://releasenotes.melissa.com/on-premise-api/presort-object/
+
+The console will ask the user for:
+
+- A csv file that contains addresses you would like to presort
+
+And return 
+
+- A csv file of presorted addresses
+
+## Tested Environments
+
+- Windows 64-bit .NET 7.0, .NET 6.0, .NET 5.0, .NET Core 3.1
+- Powershell 5.1
+- Melissa data files for 2023-02
+
+## Required File(s) and Programs
+
+#### mdPresort.dll
+This is the c++ code of the Melissa Data Object.
+
+#### Data File(s) 
+- mdL004A.dat
+- mdL004B.dat
+- mdL004C.dat
+- mdL005.dat
+- mdL006A.dat
+- mdL006B.dat
+- mdL007.dat
+- mdL008.dat
+- mdL009A.dat
+- mdL009B.dat
+- mdL010.dat
+- mdL011A.dat
+- mdL011B.dat
+- mdL012.dat
+- mdL201A.dat
+- mdL201B.dat
+- mdL601.dat
+- mdL602.dat
+- mdL801A.dat
+- mdL801B.dat
+- mdPresort.lic
+- mdValid5.dat
+- PostalRatesFile.txt
+- 3600r_T.pdf
+- 3602c_T.pdf
+- 3602n_T.pdf
+- 3602r_T.pdf
+- 8125_T.pdf
+- mdCityState.dat
+- mdDSF.DAT
+- mdL001.dat
+- mdL002A.dat
+- mdL002B1.dat
+- mdL002B2.dat
+- mdL002C.dat
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+This project is compatible with .NET 7.0, .NET 6.0, .NET 5.0, and .NET Core 3.1. If you would like to run this project for any other version besides .NET 7.0, proceed with the following procedures but check for and download your desired .NET version.
+
+#### Install the Dotnet Core SDK
+Before starting, make sure that the .NET 7.0 SDK has been correctly installed on your machine (If you have Visual Studio installed, you most likely have it already). If you are unsure, you can check by opening a command prompt window and typing the following:
+
+`dotnet --list-sdks`
+
+If the .NET 7.0 SDK is already installed, you should see it in the following list:
+
+![alt text](/screenshots/dotnet_output.png)
+
+As long as the above list contains version `7.0.xxx` (underlined in red), then you can skip to the next step. If your list does not contain version 7.0, or you get any kind of error message, then you will need to download and install the .NET 7.0 SDK from the Microsoft website.
+
+To download, follow this link: https://dotnet.microsoft.com/en-us/download/dotnet
+
+Select `.NET 7.0` and you will be navigated to the download page.
+
+Click and download the `x64` SDK installer for your operating system.
+
+(IMPORTANT: Make sure you download the SDK, NOT the runtime. the SDK contains both the runtime as well as the tools needed to build the project.)
+
+![alt text](/screenshots/net7.png)
+
+Once clicked, your web browser will begin downloading an installer for the SDK. Run the installer and follow all of the prompts to complete the installation (your computer may ask you to restart before you can continue). Once all of that is done, you should be able to verify that the SDK is installed with the `dotnet --list-sdks` command.
+
+#### Set up Powershell settings
+If running Powershell for the first time, you will need to run this command in the Powershell console: `Set-ExecutionPolicy RemoteSigned`.
+The console will then prompt you with the following warning shown in the image below. 
+ - Enter `'A'`. 
+ 	- If successful, the console will not output any messages. (You may need to run Powershell as administrator to enforce this setting).
+	
+ ![alt text](/screenshots/powershell_executionpolicy.png)
+
+----------------------------------------
+
+#### Download this project
+```
+$ git clone https://github.com/MelissaData/PresortObject-Dotnet
+$ cd PresortObject-Dotnet
+```
+
+#### Set up Melissa Updater 
+Melissa Updater is a CLI application allowing the user to update their Melissa applications/data. 
+
+- Download Melissa Updater here: <https://releases.melissadata.net/Download/Library/WINDOWS/NET/ANY/latest/MelissaUpdater.exe>
+- Create a folder within the cloned repo called `MelissaUpdater`.
+- Put `MelissaUpdater.exe` in `MelissaUpdater` folder you just created.
+
+----------------------------------------
+
+#### Different ways to get data file(s)
+1.  Using Melissa Updater
+	- It will handle all of the data download/path and dll(s) for you. 
+2.  If you already have the latest DQS Release (ZIP), you can find the data file(s) and dll(s) in there
+	- Use the location of where you copied/installed the data and update the "$DataPath" variable in the powershell script.
+	- Copy all the dll(s) mentioned above into the `MelissaPresortObjectWindowsDotnet` project folder.
+	
+----------------------------------------
+
+#### Configure Target Framework
+Depending on your target .NET framework, you may need to configure the powershell script. In order to do this, open up the `MelissaPresortObjectWindowsDotnet.ps1` for editing, proceed to the bottom of the script where you will find this section of code.
+
+Default set for .NET 7.0
+```
+dotnet publish -f="net7.0" -c Release -o $BuildPath MelissaPresortObjectWindowsDotnet\MelissaPresortObjectWindowsDotnet.csproj
+#dotnet publish -f="net6.0" -c Release -o $BuildPath MelissaPresortObjectWindowsDotnet\MelissaPresortObjectWindowsDotnet.csproj
+#dotnet publish -f="net5.0" -c Release -o $BuildPath MelissaPresortObjectWindowsDotnet\MelissaPresortObjectWindowsDotnet.csproj
+#dotnet publish -f="netcoreapp3.1" -c Release -o $BuildPath MelissaPresortObjectWindowsDotnet\MelissaPresortObjectWindowsDotnet.csproj
+```
+The target framework is specified with the -f flag found in the command line. If you wish to use any version besides .NET 7.0, please uncomment the line containing that framework and comment out the line containing the .NET 7.0 framework (# to comment).
+
+## Run Powershell Script
+Parameters:
+- -file: a test file
+ 	
+  This is convenient when you want to get results for a specific file in one run instead of testing multiple files in interactive mode.
+
+- -license (optional): a license string to test the Presort Object
+- -quiet (optional): add to the command if you do not want to get any console output from the Melissa Updater
+
+When you have modified the script to match your data location, let's run the script. There are two modes:
+- Interactive 
+
+    The script will prompt the user for a file path, then use the provided file to test Presort Object.  For example:
+    ```
+    $ .\MelissaPresortObjectWindowsDotnet.ps1
+    ```
+    For quiet mode:
+    ```
+    $ .\MelissaPresortObjectWindowsDotnet.ps1 -quiet
+    ```
+- Command Line 
+
+    You can pass a file path in ```-file``` parameter and a license string in ```-license``` parameter to test Presort Object. For example:
+    ```
+    $ .\MelissaPresortObjectWindowsDotnet.ps1 -file ".\MelissaPresortObjectWindowsDotnet\MelissaPresortObjectSampleInput.csv" 
+    $ .\MelissaPresortObjectWindowsDotnet.ps1 -file ".\MelissaPresortObjectWindowsDotnet\MelissaPresortObjectSampleInput.csv" -license "<your_license_string>"
+    ```
+    For quiet mode:
+    ```
+    $ .\MelissaPresortObjectWindowsDotnet.ps1 -file ".\MelissaPresortObjectWindowsDotnet\MelissaPresortObjectSampleInput.csv" -quiet
+    $ .\MelissaPresortObjectWindowsDotnet.ps1 -file ".\MelissaPresortObjectWindowsDotnet\MelissaPresortObjectSampleInput.csv" -license "<your_license_string>" -quiet
+    ```
+This is the expected output from a successful setup for interactive mode:
+
+![alt text](/screenshots/output.png)
+
+    
+## Troubleshooting
+
+Troubleshooting for errors found while running your program.
+
+### C# Errors:
+
+| Error      | Description |
+| ----------- | ----------- |
+| ErrorRequiredFileNotFound      | Program is missing a required file. Please check your Data folder and refer to the list of required files above. If you are unable to obtain all required files through the Melissa Updater, please contact technical support below. |
+| ErrorDatabaseExpired   | .db file(s) are expired. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check powershell script for '$RELEASE_VERSION = {version}'  and change the release version if you are using an out of date release).     |
+| ErrorFoundOldFile   | File(s) are out of date. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check powershell script for '$RELEASE_VERSION = {version}'  and change the release version if you are using an out of date release).    |
+| ErrorLicenseExpired   | Expired license string. Please contact technical support below. |
+
+
+## Contact Us
+
+For free technical support, please call us at 800-MELISSA ext. 4 (800-635-4772 ext. 4) or email us at tech@melissa.com.
+
+To purchase this product, contact the Melissa sales department at 800-MELISSA ext. 3 (800-635-4772 ext. 3).
